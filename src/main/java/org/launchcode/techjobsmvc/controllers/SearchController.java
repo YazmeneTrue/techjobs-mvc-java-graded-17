5dd088087b560d38b53332a38004c39751ac9885
Yazmene <yazmenet@Yazmenes-Laptop.local>
2024-06-16 11:53:50 -0400

    task two is failing test display find all, task three is still failing three test

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -32,15 +32,16 @@ public class SearchController {
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
 
-        if ("all".equalsIgnoreCase(searchType) || searchTerm.isEmpty()) {
+        if (searchTerm.equalsIgnoreCase("all") || searchTerm.isEmpty()) {
             jobs = JobData.findAll();
         } else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         }
 
         model.addAttribute("columns", columnChoices);
-        model.addAttribute("searchType", searchType);
-        model.addAttribute("searchTerm", searchTerm);
+     model.addAttribute("searchType", searchType);
+     model.addAttribute("searchTerm", searchTerm);
+        model.addAttribute("title", "Search Condition: " + columnChoices.get(searchType) + " Search Term: " + searchTerm);
         model.addAttribute("jobs", jobs);
         return "search";
     }
